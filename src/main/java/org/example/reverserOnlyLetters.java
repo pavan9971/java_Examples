35f79951233cc8b48064f34eb1fa58335dd7d2de
pavan9971 <pavan9971@gmail.com>
2023-07-26 23:05:40 +0530

    added one more test cases

--- src/main/java/org/example/reverserOnlyLetters.java
+++ src/main/java/org/example/reverserOnlyLetters.java
@@ -4,6 +4,7 @@ public class reverserOnlyLetters {
     public static void main(String[] args){
        System.out.println(reverse("a-bC-dEf-ghIj"));
         System.out.println(reverse("ab-cd"));
+
     }
     public static String reverse(String s) {
         StringBuilder sb = new StringBuilder(s);
